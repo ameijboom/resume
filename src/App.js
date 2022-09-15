8de06f56d0fa8c2ca654c286f408cf74449e5c4a
Alwin <meijboom.am@gmail.com>
2022-09-15 21:01:00 +0200

    Add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1 className="title">Resume</h1>
+        <Header />
       </div>
     );
   }
